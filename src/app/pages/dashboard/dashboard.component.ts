cfd7447010aafc7c05c4169d54bde29e1d701af2
Moises <jhordyaguirrebalbin@gmail.com>
2024-09-15 01:14:24 -0500

    avance dashboard paciente

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,18 +2,18 @@ import { Component, ChangeDetectorRef, inject, OnInit } from '@angular/core';
 import { CommonModule, formatDate } from '@angular/common';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from 'src/app/components/ui/header/header.component';
-import { TerapiaService } from 'src/app/services/terapia/terapia.service';
 import { ActivatedRoute } from '@angular/router';
 import { ThemeService } from 'src/app/services/theme.service';
 import { AgGridAngular } from 'ag-grid-angular';
 import { ColDef, GridReadyEvent, GridApi } from 'ag-grid-community';
 import { AG_GRID_LOCALE_ES } from '@ag-grid-community/locale';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoadingService } from 'src/app/services/loading.service';
 import { map, Observable, take } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { PacienteService } from 'src/app/services/paciente/paciente.service';
 import { IPaciente } from 'src/app/models/paciente';
+import { Cita } from 'src/app/models/cita';
+import { CitaService } from 'src/app/services/citas/cita.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 
 @UntilDestroy({ checkProperties: true })
@@ -32,26 +32,24 @@ export class DashboardComponent implements OnInit {
   theme = inject(ThemeService);
   pacienteService = inject(PacienteService);
   pacientesList: Observable<IPaciente[]> = new Observable();
+  citasPaciente: Observable<Cita[]> = new Observable(); // Observable para las citas del paciente seleccionado
+  selectedPacienteId: string | null = null; // Almacena el ID del paciente seleccionado
+
   colDefs: ColDef[] = [
-    { field: 'nombre', headerName: 'Nombres', filter: true },
-    { field: 'dni', headerName: 'DNI', filter: true },
-    { field: 'genero.nombre', headerName: 'Género' },
-    {
-      field: 'fecha_nacimiento',
-      headerName: 'Fecha Nacimiento',
-      filter: 'agDateColumnFilter',
-      valueFormatter: (params) => formatDate(params.value, 'dd/MM/yyyy', 'en'),
-    },
-    { field: 'user.name', headerName: 'Padre de familia', filter: true },
-    { field: 'direccion', headerName: 'Dirección', filter: true },
-    { field: 'colegio', headerName: 'Colegio', filter: true },
+    { field: 'paciente.nombre', headerName: 'Paciente', filter: true },
+    //{ field: 'sesion.fecha_inicio', headerName: 'Fecha Inicio', filter: true, valueFormatter: (params) => formatDate(params.value, 'dd/MM/yyyy', 'en') },
+    //{ field: 'sesion.hora_inicio', headerName: 'Hora Inicio', filter: true },
+    //{ field: 'sesion.hora_fin', headerName: 'Hora Fin', filter: true },
+    { field: 'terapia.nombre', headerName: 'Terapia', filter: true },
+    { field: 'sede.nombre', headerName: 'Sede', filter: true },
   ];
 
 
   constructor(
     private route: ActivatedRoute,
     private changeDetector: ChangeDetectorRef,
-    private router: Router
+    private router: Router,
+    private citaService: CitaService
   ) {}
 
   ngOnInit() {
@@ -65,13 +63,33 @@ export class DashboardComponent implements OnInit {
     );
   }
 
+  // Cargar citas del paciente seleccionado
+  loadCitasPaciente(idPaciente: string) {
+    this.citasPaciente = this.citaService.getCitasByPaciente(idPaciente).pipe(
+      map((resp: { data: Cita[] }) => resp.data), // Tipar la respuesta correctamente
+      untilDestroyed(this)
+    );
+  }
+
   handleSelectChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     const selectedValue = selectElement.value;
 
     if (selectedValue) {
-      // Implementa la lógica que desees para cuando se seleccione un paciente
-      this.router.navigate([selectedValue], { relativeTo: this.route });
+      // Busca el paciente seleccionado en la lista de pacientes
+      this.pacientesList.pipe(take(1)).subscribe((pacientes) => {
+        const pacienteSeleccionado = pacientes.find(p => p.id_paciente === selectedValue);
+
+        if (pacienteSeleccionado) {
+          // Cargar citas del paciente seleccionado
+          this.loadCitasPaciente(pacienteSeleccionado.id_paciente);
+
+          // Luego, establece los datos en el grid
+          this.citasPaciente.pipe(take(1)).subscribe((citas) => {
+            this.gridApi.applyTransaction({ add: citas });
+          });
+        }
+      });
     }
   }
 
@@ -89,6 +107,6 @@ export class DashboardComponent implements OnInit {
     });
 
     resizeObserver.observe(document.body);
-    handleResize(); // Llamada inicial
+    handleResize();
   }
 }
