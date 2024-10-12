e5ff361342a56203ad817f6e835b0cc3b2e43301
yosel1989 <yosel1989@gmail.com>
2024-10-12 11:45:43 -0500

    12102024 1145

--- dist/public/browser/chunk-PCDKIPJS.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/public/browser/chunk-YWRKPUSH.js
-import{a as p}from"./chunk-MTDZXKVK.js";import{m as n}from"./chunk-VUZMLSUX.js";import{sa as o,ya as e}from"./chunk-UAHLOU27.js";var m=(()=>{let t=class t{constructor(r){this.http=r,this.apiUrl=p.apiUrl}getAll(){return this.http.get(`${this.apiUrl}/genero/list`)}};t.\u0275fac=function(a){return new(a||t)(e(n))},t.\u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"});let i=t;return i})();export{m as a};
-========
-import{a as p}from"./chunk-4PY7PIQX.js";import{m as n}from"./chunk-VUZMLSUX.js";import{sa as o,ya as e}from"./chunk-UAHLOU27.js";var m=(()=>{let t=class t{constructor(r){this.http=r,this.apiUrl=p.apiUrl}getAll(){return this.http.get(`${this.apiUrl}/genero/list`)}};t.\u0275fac=function(a){return new(a||t)(e(n))},t.\u0275prov=o({token:t,factory:t.\u0275fac,providedIn:"root"});let i=t;return i})();export{m as a};
->>>>>>>> 9aeaa5d7211eb27815509e04c0042dbff28c2c2e:dist/public/browser/chunk-PCDKIPJS.js
