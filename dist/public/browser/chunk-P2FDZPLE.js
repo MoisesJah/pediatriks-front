596573b9d8c5c2a41dbc88e7506150f7dfd438ed
yosel1989 <yosel1989@gmail.com>
2024-11-10 19:39:45 -0500

    delete: remove dist

--- dist/public/browser/chunk-P2FDZPLE.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c,d}from"./chunk-EPQZSVFD.js";import"./chunk-D6LKZNPK.js";export{c as AgGridAngular,d as AgGridModule,a as AngularFrameworkComponentWrapper,b as AngularFrameworkOverrides};
