566f736deb59f7cba57a7734b330eb03544511f7
MaycolGallo <maycolgallo25@gmail.com>
2025-01-17 16:53:17 -0500

    cambios fichas pdf admin

--- src/app/pages/terapista/fichas/fichas-result/fichas-result.component.ts
+++ src/app/pages/terapista/fichas/fichas-result/fichas-result.component.ts
@@ -10,6 +10,10 @@ import 'survey-core/i18n/spanish';
 import { AuthService } from 'src/app/services/auth.service';
 import { SurveyPDF } from 'survey-pdf';
 import { footer1, logo } from 'src/app/utils/logo';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { vfs } from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 
 function createSurveyPdfModel(body: any, survey: Model) {
   const pdfWidth = !!survey && survey.pdfWidth ? survey.pdfWidth : 210;
@@ -34,7 +38,7 @@ function createSurveyPdfModel(body: any, survey: Model) {
     await canvas.drawImage({
       base64: logo,
       width: 100,
-      
+
       // horizontalAlign: 'left',
       // width: (canvas.rect.yBot - canvas.rect.yTop) * 3,
       // height: (canvas.rect.yBot - canvas.rect.yTop) * 1.5,
@@ -48,18 +52,17 @@ function createSurveyPdfModel(body: any, survey: Model) {
       fontSize: 10,
     });
     await canvas.drawImage({
-      base64:footer1,
+      base64: footer1,
       height: 520,
       width: 210,
 
       rect: {
         // xLeft: 0,
-
         // yTop: 0,
         // xRight: canvas.rect.xRight,
         // yBot: canvas.rect.yBot
-      }      
-    })
+      },
+    });
     canvas.drawText({
       text: 'Pagina ' + canvas.pageNumber + ' de ' + canvas.pageCount,
       fontSize: 10,
@@ -107,7 +110,71 @@ export class FichasResultComponent implements OnInit {
       this.survey.addNavigationItem({
         id: 'pdf',
         title: 'Guardar como PDF',
-        action: (c) => this.savePdf(res.data.ficha.body),
+        action: () => this.generatePDF(),
+        // action: () => {
+        //   const pdfData = [];
+        //   console.log(JSON.parse(res.data.body));
+        //   Object.keys(JSON.parse(res.data.body)).forEach((key) => {
+        //     const question = this.survey.getQuestionByName(key);
+
+        //     const answer = this.survey.data[key];
+
+        //     if (question?.getType() === 'matrix') {
+        //       const matrixAnswer = [];
+        //       const rows = question?.rows.filter(
+        //         (row) => typeof row === 'object'
+        //       );
+        //       console.log('rows', rows);
+        //       rows.forEach((row, rowIndex) => {
+        //         const rowAnswer = [];
+        //         console.log(question?.columns);
+        //         question?.columns.forEach((column, columnIndex) => {
+        //           const cellAnswer = answer[rowIndex][columnIndex];
+        //           rowAnswer.push(cellAnswer);
+        //         });
+        //         matrixAnswer.push(rowAnswer);
+        //       });
+        //       pdfData.push({
+        //         question: question?.title,
+        //         answer: matrixAnswer,
+        //       });
+        //     } else {
+        //       pdfData.push({
+        //         question: question?.title,
+        //         answer: answer,
+        //       });
+        //     }
+        //   });
+
+        //   const docDefinition = {
+        //     content: pdfContent.map((item) => {
+        //       if (Array.isArray(item.answer)) {
+        //         const matrixTable = {
+        //           table: {
+        //             headerRows: 1,
+        //             widths: ['*', '*', '*', '*', '*'],
+        //             body: [question.columns.map((column) => column.text)],
+        //           },
+        //         };
+        //         item.answer.forEach((row) => {
+        //           matrixTable.table.body.push(row);
+        //         });
+        //         return matrixTable;
+        //       } else {
+        //         return { text: `${item.question}: ${item.answer}` };
+        //       }
+        //     }),
+        //   };
+
+        //   const newPdfMake = Object.assign({}, pdfMake);
+        //   newPdfMake.vfs = pdfFonts;
+
+        //   // Create the PDF document
+        //   const pdfDocGenerator = newPdfMake.createPdf(docDefinition);
+
+        //   // Download the PDF document
+        //   pdfDocGenerator.download();
+        // },
       });
 
       this.survey.mergeData(res.data.body);
@@ -130,8 +197,218 @@ export class FichasResultComponent implements OnInit {
       });
     });
   }
-  savePdf(body:any) {
-    console.log(body);
-    createSurveyPdfModel(body, this.survey).save('ficha.pdf');
+
+  generatePDF() {
+    const surveyData = this.survey.data;
+
+    const docDefinition: TDocumentDefinitions = {
+      header: [
+        {
+          columns: [
+            {
+              image: logo,
+              width: 180,
+              height: 45,
+              margin: [10, 10, 10, 0],
+            },
+            {
+              text: '',
+              width: '*', // takes up the remaining space
+              fillAvailableSpace: true,
+            },
+            {
+              text: this.survey.title,
+              fontSize: 11,
+              alignment: 'right',
+              width: 100, // sets the maximum width to 100px equivalent
+              margin: [10, 10, 10, 0],
+            },
+          ],
+          columnGap: 10,
+          width: '100%',
+        },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 0,
+              x2: '100%',
+              y2: 0,
+              lineWidth: 2,
+              color: '#ADD8E6', // light blue
+              lineDashPattern: [70, 30], // 70% light blue, 30% purple
+              lineDashColor: '#7A288A', // purple
+            },
+          ],
+          margin: [0, -5, 0, 0], // adjust the margin to position the line under the logo and title
+        },
+      ],
+      footer: function(currentPage, pageCount) {
+        return {
+          canvas: [
+            {
+              type: 'rect',
+              x: 0,
+              y: 0,
+              w: '100%',
+              h: 100,
+              color: '#f7f7f7' // background color
+            }
+          ],
+          columns: [
+            {
+              text: [
+                'Text 1',
+                'Text 2',
+                'Text 3'
+              ].join('\n'),
+              fontSize: 12,
+              margin: [10, 20, 0, 0]
+            },
+            {
+              text: `Page ${currentPage} of ${pageCount}`,
+              fontSize: 12,
+              alignment: 'right',
+              margin: [0, 20, 10, 0]
+            }
+          ],
+          columnGap: 10,
+          width: '100%'
+        };
+      },
+      content: [
+        // { text: this.survey.title, style: 'header' },
+        // // { text: `Generated on: ${timestamp}`, style: 'subheader' },
+        // { text: '\n' },
+        {
+          text: this.survey.title,
+          style: 'header',
+        },
+        {
+          text: this.survey.description,
+          style: 'subheader',
+        },
+        // ...this.generatePDFContent(surveyData)
+      ],
+      styles: {
+        header: {
+          fontSize: 22,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        subheader: {
+          fontSize: 14,
+          bold: true,
+          margin: [0, 0, 0, 20],
+        },
+        questionTitle: {
+          fontSize: 12,
+          bold: true,
+          margin: [0, 10, 0, 5],
+        },
+        answer: {
+          fontSize: 11,
+          margin: [0, 0, 0, 10],
+        },
+      },
+    };
+    this.survey.getAllQuestions().forEach((question) => {
+      docDefinition.content.push({
+        text: question.title,
+        style: 'question',
+      });
+
+      console.log(question.type);
+
+      switch (question.getType()) {
+        case 'text':
+          docDefinition.content.push({
+            text: this.survey.data[question.name],
+            style: 'answer',
+          });
+          break;
+        case 'checkbox':
+          const checkboxAnswers = this.survey.data[question.name];
+          checkboxAnswers.forEach((answer) => {
+            docDefinition.content.push({
+              text: answer,
+              style: 'answer',
+            });
+          });
+          break;
+        case 'radiogroup':
+          docDefinition.content.push({
+            text: this.survey.data[question.name],
+            style: 'answer',
+          });
+          break;
+        case 'matrix':
+          if (this.survey.data[question.name]) {
+            const table = {
+              table: {
+                headerRows: 1,
+                widths: ['*', ...Array(question.columns.length).fill('*')],
+                body: [
+                  [''].concat(question.columns.map((column) => column.text)),
+                ],
+              },
+            };
+            question.rows.forEach((row) => {
+              const rowData = [row.text];
+              question.columns.forEach((column) => {
+                rowData.push(
+                  this.survey.data[question.name][row.value] === column.value
+                    ? '•'
+                    : ''
+                );
+              });
+              table.table.body.push(rowData);
+            });
+            docDefinition.content.push(table);
+          }
+          break;
+        default:
+          console.log(`Unsupported question type: ${question.type}`);
+      }
+    });
+    const newPdfMake = Object.assign({}, pdfMake);
+    newPdfMake.vfs = pdfFonts;
+    newPdfMake.createPdf(docDefinition).download('survey-responses.pdf');
   }
+  generatePDFContent(data: any) {
+    const content: any[] = [];
+
+    Object.entries(data).forEach(([key, value]) => {
+      const question = this.survey.getQuestionByName(key);
+      if (question) {
+        content.push({ text: question.title, style: 'questionTitle' });
+
+        if (Array.isArray(value)) {
+          content.push({
+            text: value.join(', '),
+            style: 'answer',
+          });
+        } else if (typeof value === 'object') {
+          Object.entries(value).forEach(([rowKey, rowValue]) => {
+            content.push({
+              text: `${rowKey}: ${rowValue}`,
+              style: 'answer',
+            });
+          });
+        } else {
+          content.push({
+            text: value.toString(),
+            style: 'answer',
+          });
+        }
+      }
+    });
+
+    return content;
+  }
+  // savePdf(body: any) {
+  //   console.log(body);
+  //   createSurveyPdfModel(body, this.survey).save('ficha.pdf');
+  // }
 }
