21f2bf9fd1fe1f4c5e18272dd67e5464506a3cce
MaycolGallo <maycolgallo25@gmail.com>
2025-03-17 21:38:55 -0500

    admin paciente stats

--- src/app/models/paciente.ts
+++ src/app/models/paciente.ts
@@ -1,12 +1,13 @@
-import { IUser } from "./user";
+import { IUser } from './user';
 
 export interface IPaciente {
-    id_paciente: string;
-    id: number;
-    dni: string;
-    nombre: string;
-    fecha_nacimiento: Date;
-    direccion: string;
-    genero: string;
-    user?: IUser
+  id_paciente: string;
+  id: number;
+  dni: string;
+  nombre: string;
+  diagnostico: string;
+  fecha_nacimiento: Date;
+  direccion: string;
+  genero: string;
+  user?: IUser;
 }
