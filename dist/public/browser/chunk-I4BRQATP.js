0e2867f93f9bf7632f28763c73fdccdc3ef8a71c
yosel1989 <yosel1989@gmail.com>
2025-03-27 17:44:32 -0500

    build

---
+++ dist/public/browser/chunk-I4BRQATP.js
@@ -0,0 +1,1 @@
+import{a,b,c,d}from"./chunk-XKOQY6PJ.js";import"./chunk-B762IU3Y.js";export{c as AgGridAngular,d as AgGridModule,a as AngularFrameworkComponentWrapper,b as AngularFrameworkOverrides};
